f2472354f56bf47bbded0a68f4542d8065b1893b
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-26 17:22:41 +0200

    [FEAT] aggiunto filtraggio wheredate dato odierna

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -11,7 +11,9 @@ class PageController extends Controller
 {
     public function index(){
         // $trains = Train::all();
-        $trains = Train::whereDate('orario_partenza', '=','2023-07-26')->get();
+        $trains = Train::whereDate('orario_partenza', '=','2023-07-26')->get(); //con data statica
+
+        // Oppure per avere la data di oggi dinamica ->  $trains = Train::whereDate('orario_partenza', '=', Carbon::now())->get();
         return view('home', compact('trains'));
     }
 }
