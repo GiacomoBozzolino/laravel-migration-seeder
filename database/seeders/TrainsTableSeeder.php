a6b2f0206320085fc77c8e5a33991c49fde7c911
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-27 14:36:38 +0200

    [FEAT] compilazione fseed con faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Faker\Provider\en_US\Person;
+
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +16,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<10; $i++){
+            $train = new Train();
+            $train->azienda=$faker->word();
+            $train->stazione_partenza=$faker->word();
+            $train->stazione_arrivo=$faker->word();
+            $train->orario_partenza=$faker->dateTimeThisMonth();
+            $train->orario_arrivo=$faker->dateTimeThisMonth();
+            $train->codice_treno=$faker->word();
+            $train->numero_carrozze=$faker->numberBetween(1, 15);
+            $train->in_orario=$faker->boolean();
+            $train->cancellato=$faker->boolean();
+
+            $train->save();
+
+
+        }
     }
 }
